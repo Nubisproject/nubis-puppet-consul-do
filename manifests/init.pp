1cd7d3c05557ca765f67fd81873ff6bd3dc4b853
limed <limed@mozilla.com>
2016-01-26 14:53:26 -0800

    consul_do cleanup and documenting

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,21 +4,12 @@
 #
 # === Parameters
 #
-# Document parameters here.
+# version - specify the version of consul_do
 #
 # [*version*]
 #   Installs a specific version of consul_do
 #
 # === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
 #
 # === Examples
 #
@@ -29,11 +20,7 @@
 # === Authors
 #
 # Ed Lim <limed@mozilla.com>
-#
-# === Copyright
-#
-# Copyright 2015 Your name here, unless otherwise noted.
-#
+
 class consul_do (
     $version
 ) {
